2b67b1e5ba7aae9f12f8ddcd26a0186192c7c384
Bartosz Kaszowski <noespa@gmail.com>
2023-12-06 14:28:05 +0100

    Create a registration form, register controller and view, implements session

---
+++ controllers/registration/store.php
@@ -0,0 +1,45 @@
+<?php
+use Core\Database;
+use Core\Validator;
+use Core\App;
+
+$db = App::resolve(Database::class);
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$errors = [];
+if (!Validator::email($email)) {
+   $errors['email'] = 'Please provide a valid email address.';
+}
+
+if (!Validator::string($password, 7, 30)) {
+    $errors['password'] = 'Please provide a password of at least seven characters.';
+}
+
+if (!empty($errors)) {
+    return view('registration/create.view.php', [
+        'errors' => $errors
+    ]);
+}
+
+$user = $db->query('select * from users where email = :email', [
+    'email' => $email
+])->find();
+
+if($user) {
+    header('location: /');
+    exit();
+} else {
+    $db->query('INSERT INTO users(email, password) VALUES(:email, :password)', [
+        'email' => $email,
+        'password' => password_hash($password, PASSWORD_DEFAULT)
+    ]);
+
+    $_SESSION['user'] = [
+        'email' => $email
+    ];
+
+    header('location: /');
+    exit();
+}
